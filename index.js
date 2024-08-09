297b819b763b30ba81866d20cf18708ea440c65b
VictorMGC2912 <victor89_13@hotmail.com>
2024-08-09 16:18:07 +0200

    añadir movies en data

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log('Hola mundo!');
+//import { movie } from "/dataMovie.js";
+
+function createPosterElement(poster) {
+    const element = document.createElement("img");
+    element.src = poster;
+    element.className = 'movie-poster';
+    return element;
+  }
+  
+  function createTitleElement(title) {
+    const element = document.createElement("div");
+    element.className = "movie-title";
+    element.textContent = title;
+    return element;
+  }
+  
+  function createDataElement(rating, year) {
+    const element = document.createElement("div");
+    element.className = "movie-data";
+    element.textContent = `Rating: ${rating} | ${year}`;
+    return element;
+  }
+  
+  function createMovieElement(movieObj) {
+    const movieElement = document.createElement("div");
+    movieElement.className = "movie";
+    movieElement.appendChild(createPosterElement(movieObj.poster));
+    movieElement.appendChild(createTitleElement(movieObj.title));
+    movieElement.appendChild(createDataElement(movieObj.rating, movieObj.year));
+    return movieElement;
+  }
+  
+  const movieContainer = document.createElement("div");
+  movieContainer.className = "movie-container";
+  
+  const movieElement = createMovieElement(movie);
+  movieContainer.appendChild(movieElement);
+  
+  document.body.appendChild(movieContainer);
